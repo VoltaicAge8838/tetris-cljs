16d37fb895250e37b0c1c4352233244153357442
VoltaicAge8838 <brocam@gmail.com>
2016-02-02 20:33:16 -0700

    Added basic text support.

--- src/tetris_cljs/core.cljs
+++ src/tetris_cljs/core.cljs
@@ -5,8 +5,5 @@
 
 (println "Hello world!" " I like toast!")
 
-(graphics/draw-rect-old 30 500 300 440)
-
-(println (graphics/color 203 101 3 4))
-
-(graphics/draw-rect [20 20 10 150] (graphics/color 200 0 0 1) (graphics/color 0 50 0 1) 5)
+(graphics/draw :rect [20 20 100 150] (graphics/color 200 0 0 1) (graphics/color 0 50 0 1) 5)
+(graphics/draw :text ["Hello World!" 10 15] (graphics/color 0 200 0 1) (graphics/color 255 5 255 1) 1)
--- src/tetris_cljs/graphics.cljs
+++ src/tetris_cljs/graphics.cljs
@@ -1,19 +1,16 @@
 (ns tetris-cljs.graphics)
 
-(defn get-canvas []
-  (.getElementById js/document "cvs"))
+(def canvas (.getElementById js/document "cvs"))
 
-(defn get-context []
-  (.getContext (get-canvas) "2d"))
+(def context (.getContext canvas "2d"))
 
 (defn clear-canvas []
-  (do (.save (get-context))
-      (.clearRect (get-context))
-      (.restore (get-context))))
+  (do (.save context)
+      (.clearRect context)
+      (.restore context)))
 
 (defn fitCanvasToWindow []
-  (let [canvas (get-canvas)
-        newWidth (/ (* (.innerHeight js/window) (.width canvas)) (.height canvas))]
+  (let [newWidth (/ (* (.innerHeight js/window) (.width canvas)) (.height canvas))]
     (aset canvas "style" "width"
       (if (< (.innerWidth js/window) newWidth)
         "100%"
@@ -23,24 +20,19 @@
   (str "rgb(" r "," g "," b "," a ")"))
 
 (def red "rgb(200,0,0)")
-; (def draw-functions
-;   { :fill-rect .fillRect
-;     :stroke-rect .strokeRect
-;     :fill-text .fillText
-;     :stroke-text .strokeText})
 
-(defn draw-rect-old [x y w h]
-  (let [ctx (get-context)]
-    (aset ctx "fillStyle" "rgb(200,0,0)")
-    (.fillRect ctx x y w h)))
+(def draw-type
+  { :rect [ #(.fillRect context %1 %2 %3 %4)
+            #(.strokeRect context %1 %2 %3 %4)]
+    :text [ #(.fillText context %1 %2 %3)
+            #(.strokeText context %1 %2 %3)]})
 
-(defn draw-rect [args fill-color stroke-color line-width]
-  (let [ctx (get-context)]
-    (do
-      (.save ctx)
-      (aset ctx "fillStyle" fill-color)
-      (aset ctx "strokeStyle" stroke-color)
-      (aset ctx "lineWidth" line-width)
-      (apply #(.fillRect ctx %1 %2 %3 %4) args)
-      (apply #(.strokeRect ctx %1 %2 %3 %4) args)
-      (.restore ctx))))
+(defn draw [type args fill-color stroke-color line-width]
+  (do
+    (.save context)
+    (aset context "fillStyle" fill-color)
+    (aset context "strokeStyle" stroke-color)
+    (aset context "lineWidth" line-width)
+    (aset context "font" "12px Arial")
+    (doall (map #(apply % args) (type draw-type)))
+    (.restore context)))

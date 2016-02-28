79569310360d0877829377be3db164c7e52eb771
VoltaicAge8838 <brocam@gmail.com>
2016-02-27 17:14:05 -0700

    Modified code to use more human-friendly piece -grids. Tested more of the code.

--- src/tetris_cljs/tetriminoes.cljs
+++ src/tetris_cljs/tetriminoes.cljs
@@ -11,74 +11,80 @@
   (* 900 (- 1 (/ lvl (+ 2 lvl)))))
 
 (def piece-types
-  { :Z [[[1 1] [0 1] [0 0] [-1 0]]
-        [[0 1] [0 0] [1 0] [1 -1]]]
-    :S [[[-1 1] [0 1] [0 0] [1 0]]
-        [[1 1] [0 0] [1 0] [0 -1]]]
-    :O [[[0 1] [-1 1] [-1 0] [0 0]]]
-    :I [[[-1 0] [0 0] [1 0] [-2 0]]
-        [[0 2] [0 1] [0 0] [0 -1]]]
-    :T [[[0 1] [-1 0] [0 0] [1 0]]
-        [[0 1] [0 0] [-1 0] [0 -1]]
-        [[-1 0] [0 0] [1 0] [0 -1]]
-        [[0 1] [1 0] [0 0] [0 -1]]]
-    :L [[[-1 1] [-1 0] [0 0] [1 0]]
-        [[0 1] [0 0] [0 -1] [-1 -1]]
-        [[-1 0] [0 0] [1 0] [1 -1]]
-        [[1 1] [0 1] [0 0] [0 -1]]]
-    :J [[[1 1] [-1 0] [0 0] [1 0]]
-        [[0 1] [0 0] [-1 1] [0 -1]]
-        [[-1 0] [0 0] [1 0] [-1 -1]]
-        [[0 1] [0 0] [1 -1] [0 -1]]]})
+  { :z [[1 1 0]
+        [0 1 1]]
+    :s [[0 1 1]
+        [1 1 0]]
+    :o [[1 1]
+        [1 1]]
+    :i [[0 0 0 0]
+        [1 1 1 1]
+        [0 0 0 0]]
+    :t [[0 0 0]
+        [1 1 1]
+        [0 1 0]]
+    :l [[0 0 0]
+        [1 1 1]
+        [1 0 0]]
+    :j [[0 0 0]
+        [1 1 1]
+        [0 0 1]]})
 
 (defn random-pieces-list []
   (shuffle (vals piece-types)))
+
+(defn compute-score [lines-cleared level]
+  (apply * 50 (+ level 1) (range 2 lines-cleared)))
 
 (defn neighbor-direction [base n?]
   (let [dif [ (- (first base) (first n?))
               (- (last base) (last n?))]]
     (#{[0 -1] [0 1] [-1 0] [1 0]} dif)))
 
-(defn compute-score [lines-cleared level]
-  (apply * 50 (+ level 1) (range 2 lines-cleared)))
+(defn piece->coords [piece]
+  (reduce-kv
+    (fn [yo y row]
+      (let [result (reduce-kv
+                    (fn [xo x cell]
+                      (cond-> xo
+                        (= 1 cell) (conj [x y])))
+                    [] row)]
+        (cond-> yo
+          (not-empty result) (into conj result))))
+    [] (:blocks piece)))
 
-(defn piece->blocks [piece]
+(defn piece->neighbors [piece]
   (let [color (:color piece)
-        coord-list (:coords piece)]
+        coord-list (piece->coords piece)]
     (for [x coord-list
           :let [neighbors
                 (for [y coord-list
                       :let [dir (neighbor-direction x y)]
-                      :when dir
-                      dir])]
-          { :neighbors neighbors
-            :color color}])))
+                      :when dir]
+                  dir)]]
+      { :neighbors (apply hash-set (conj neighbors [0 -1]))
+        :color color})))
 
-(defn make-active-piece [color type coords]
+(defn make-piece [color type]
   { :color color
-    :type type
-    :coords coords})
+    :blocks (piece-types type)})
 
-(defn make-piece [color type & [rotation]]
-  (let [p-type (piece-types type)]
-    (->> p-type
-      count
-      (mod (or rotation 0))
-      p-type
-      (make-active-piece color type))))
-
-(defn get-xy [x y block]
-  (-> block :coords (map + [x y])))
+(defn get-xy [x y piece]
+  (->> piece piece->coords (mapv #(mapv + [x y] %))))
 
 (defn can-add-piece [grid x y piece]
   (every?
-    #(= {} (get-in grid (get-xy x y %)))
-    piece))
+    #(= {} (get-in grid %))
+    (get-xy x y piece)))
 
 (defn place-piece [grid x y piece]
   (reduce
-    (fn [g block] (assoc-in g (get-xy x y block) (:block block)))
-    grid piece))
+    (fn [g cn] (assoc-in g (first cn) (last cn)))
+    grid (map vector (get-xy x y piece) (piece->neighbors piece))))
+
+(defn rotate-right [piece]
+  (vec (apply (partial map vector)
+          (reverse piece))))
 
 (defn make-game-state [colors]
   { :level 0
